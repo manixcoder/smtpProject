7205c2d8db71a548c98fd9778aeae5c82c36173d
Manish Kumar Pathak <pathakmanish86@gmail.com>
2020-02-07 10:17:16 +0530

    Update gcm.php

--- application/config/gcm.php
+++ application/config/gcm.php
@@ -4,8 +4,7 @@
 |--------------------------------------------------------------------------
 | Get API Key: https://code.google.com/apis/console/
 */
-$config['gcm_api_key'] = 'AIzaSyD-QfR3lQ8d-obicKd3NdrLb1_H788taGo';
-//$config['gcm_api_key'] = 'AIzaSyA3eh0DkKDH-rgRhp-VQLWrBUZUlItOvVs';
+$config['gcm_api_key'] = 'gcm_api_key';
 /*
 |--------------------------------------------------------------------------
 | API Send Address
